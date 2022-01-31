f5f782f8f4bca966a26c14da51cfcce95e939d9c
Shun Cheung <shunkongcheung@gmail.com>
2022-01-31 12:05:45 -0800

    feat(getCommonHandScore): init

--- src/services/getTileScore.ts
+++ src/services/getTileScore.ts
@@ -1,9 +1,10 @@
 import { ScoreTuple, TileType } from "../constants";
 
+import getCommonHandScore from "./getCommanHandScore";
 import getThirteenOrphansScore from "./getThirteenOrphansScore";
 
 const getTileScore = (tiles: Array<TileType>): Array<ScoreTuple> => {
-  const checkers = [getThirteenOrphansScore];
+  const checkers = [getCommonHandScore, getThirteenOrphansScore];
 
   const hands = checkers.map((checker) => checker(tiles));
   return hands.filter((itm) => itm[0] > 0);
