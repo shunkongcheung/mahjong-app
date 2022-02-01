bbb7735c5a83d3a65314e7964c8dbe1bb836644d
Shun Cheung <shunkongcheung@gmail.com>
2022-02-01 11:15:24 -0800

    feat(getSpecificTileSetFeasibility): maxCount

--- src/services/getSpecificTileSetFeasibility.ts
+++ src/services/getSpecificTileSetFeasibility.ts
@@ -4,13 +4,18 @@ const getSpecificTileSetFeasibility = (
   iOnHands: Array<TileType>,
   remains: Array<TileType>,
   tileSet: Array<TileType>, // difficulty to retrieve from remains
-  difficulty: number
+  difficulty: number,
+  maxCountPerTile = 4
 ) => {
   const feasibility = tileSet.reduce((acc, tile) => {
     if (acc === -100) return acc;
 
-    const found = !!iOnHands.find((itm) => itm === tile);
-    if (found) return acc + 1;
+    const count = iOnHands.reduce(
+      (acc, itm) => acc + (itm === tile ? 1 : 0),
+      0
+    );
+
+    acc += Math.max(count, maxCountPerTile);
 
     // maximum should have 4 out there in the remain
     const remainCount = remains.reduce(
