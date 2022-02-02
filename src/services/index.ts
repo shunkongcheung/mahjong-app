7f539960d83b762225129526504fe3c5fa8c3410
Shun Cheung <shunkongcheung@gmail.com>
2022-02-02 10:37:54 -0800

    feat(getI18N): init

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import getFeasibility from "./getFeasibility";
+import getI18N from "./getI18N";
 import getInitialTiles from "./getInitialTiles";
 import getTileScore from "./getTileScore";
 
-export { getFeasibility, getInitialTiles, getTileScore };
+export { getFeasibility, getI18N, getInitialTiles, getTileScore };
