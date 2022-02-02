355e5c254b431841fdce609694ff90957dca9650
Shun Cheung <shunkongcheung@gmail.com>
2022-02-02 12:03:41 -0800

    feat(getI18N): add more mappings

--- src/services/getI18N.ts
+++ src/services/getI18N.ts
@@ -1,6 +1,6 @@
 import * as constants from "../constants";
 
-const getI18N = (value: constants.TileType) => {
+const getI18N = (value: string) => {
   const mapping: { [x: string]: string } = {
     [constants.Wind.East]: "東",
     [constants.Wind.South]: "南",
@@ -18,15 +18,15 @@ const getI18N = (value: constants.TileType) => {
     [constants.Bamboo.Seven]: "七索",
     [constants.Bamboo.Eight]: "八索",
     [constants.Bamboo.Nine]: "九索",
-    [constants.Character.One]: "一筒",
-    [constants.Character.Two]: "二筒",
-    [constants.Character.Three]: "三筒",
-    [constants.Character.Four]: "四筒",
-    [constants.Character.Five]: "五筒",
-    [constants.Character.Six]: "六筒",
-    [constants.Character.Seven]: "七筒",
-    [constants.Character.Eight]: "八筒",
-    [constants.Character.Nine]: "九筒",
+    [constants.Character.One]: "一萬",
+    [constants.Character.Two]: "二萬",
+    [constants.Character.Three]: "三萬",
+    [constants.Character.Four]: "四萬",
+    [constants.Character.Five]: "五萬",
+    [constants.Character.Six]: "六萬",
+    [constants.Character.Seven]: "七萬",
+    [constants.Character.Eight]: "八萬",
+    [constants.Character.Nine]: "九萬",
     [constants.Dot.One]: "一筒",
     [constants.Dot.Two]: "二筒",
     [constants.Dot.Three]: "三筒",
@@ -36,6 +36,14 @@ const getI18N = (value: constants.TileType) => {
     [constants.Dot.Seven]: "七筒",
     [constants.Dot.Eight]: "八筒",
     [constants.Dot.Nine]: "九筒",
+    [constants.GameEventAction.Win]: "食",
+    [constants.GameEventAction.Kong]: "降",
+    [constants.GameEventAction.Triplet]: "碰",
+    [constants.GameEventAction.Chow]: "上",
+    [constants.GameEventAction.Flower]: "補牌",
+    [constants.GameEventAction.Pick]: "攞牌",
+    [constants.GameEventAction.Pop]: "出牌",
+    [constants.GameEventAction.NoAction]: "沒動作",
   };
 
   return mapping[value] || "不明句子";
