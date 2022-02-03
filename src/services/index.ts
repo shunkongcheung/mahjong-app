55b9b67c425a1f9d16ec9447bd052301894f803b
Shun Cheung <shunkongcheung@gmail.com>
2022-02-03 10:36:16 -0800

    feat(shuffle): init

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import getFeasibility from "./feasibility/getFeasibility";
 import getI18N from "./getI18N";
 import getTileScore from "./score/getTileScore";
+import shuffle from "./shuffle";
 
-export { getFeasibility, getI18N, getTileScore };
+export { getFeasibility, getI18N, getTileScore, shuffle };
