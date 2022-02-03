5214ddd79d8210e8954f74774973599630268920
Shun Cheung <shunkongcheung@gmail.com>
2022-02-03 10:38:14 -0800

    feat(getThrowOrder): shuffle to add uncertainity

--- src/entities/machine/getThrowOrder.ts
+++ src/entities/machine/getThrowOrder.ts
@@ -1,11 +1,12 @@
 import { Bamboo, Character, Dot, TileType } from "../../constants";
+import { shuffle } from "../../services";
 import getIsSuited from "../../services/getIsSuited";
 
 const bamboos: Array<TileType> = Object.values(Bamboo);
 const characters: Array<TileType> = Object.values(Character);
 const dots: Array<TileType> = Object.values(Dot);
 
-const sortTiles = (tiles: Array<TileType>) => {
+const sortTiles = (tiles: Array<TileType>, isByPairOnly = false) => {
   // if there are pairs, they should be further
   // or else,  the further its away from center, the earlier to throw
   tiles.sort((a, b) => {
@@ -20,6 +21,7 @@ const sortTiles = (tiles: Array<TileType>) => {
     if (aHasPair !== bHasPair) {
       return aHasPair ? 1 : -1;
     }
+    if (isByPairOnly) return 0;
 
     if (aDist > bDist) return -1;
     if (aDist < bDist) return 1;
@@ -43,14 +45,22 @@ const getThrowOrder = (tiles: Array<TileType>) => {
   const onHandDots = tiles.filter((itm) => dots.includes(itm));
   const honors = tiles.filter((itm) => !getIsSuited(itm));
 
+  // shuffle tiles, so that result is not too deterministic
+  shuffle(onHandBamboos);
+  shuffle(onHandCharacters);
+  shuffle(onHandDots);
+  shuffle(honors);
+
   // throw tile that is furthest from middle (e.g. 1 or 9)
   // as they could be risky to thirteenOrphans
   // they are less likely to form chow
   sortTiles(onHandBamboos);
   sortTiles(onHandCharacters);
   sortTiles(onHandDots);
+  sortTiles(honors, true);
 
   const suits = [onHandBamboos, onHandCharacters, onHandDots];
+  shuffle(suits);
   sortSuits(suits);
 
   // reconstruct order
