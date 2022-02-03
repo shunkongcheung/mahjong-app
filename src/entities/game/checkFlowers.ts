68405fdc8ec78da055a39bce25c7959a5ba24307
Shun Cheung <shunkongcheung@gmail.com>
2022-02-03 00:14:23 -0800

    feat(checkFlowers): return the compensated tile instead of the flower

--- src/entities/game/checkFlowers.ts
+++ src/entities/game/checkFlowers.ts
@@ -1,3 +1,4 @@
+import { TileType } from "../../constants";
 import { popFlowers, pickTiles } from "../player";
 
 import compensate from "./compensate";
@@ -9,11 +10,16 @@ const checkFlowers = (game: Game, idx: number) => {
   // pop player.onHand flower(s) to players.flowers
   const flowers = popFlowers(player);
 
-  // for however number of flower, compensate
-  for (let jdx = 0; jdx < flowers.length; jdx++)
-    pickTiles(player, compensate(game.walls));
+  const results: Array<TileType> = [];
 
-  return flowers;
+  // for however number of flower, compensate
+  for (let jdx = 0; jdx < flowers.length; jdx++) {
+    const tile = compensate(game.walls);
+    pickTiles(player, tile);
+    results.push(tile);
+  }
+
+  return results;
 };
 
 export default checkFlowers;
