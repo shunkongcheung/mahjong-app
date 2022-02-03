3f4ace7c98c08b791f5559c38f20cb9cb4155bdb
Shun Cheung <shunkongcheung@gmail.com>
2022-02-03 10:37:01 -0800

    feat(getInitialTiles): shuffle from service

--- src/entities/game/getInitialTiles.ts
+++ src/entities/game/getInitialTiles.ts
@@ -7,25 +7,7 @@ import {
   Flower,
   TileType,
 } from "../../constants";
-
-function shuffle<T>(array: Array<T>) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
+import { shuffle } from "../../services";
 
 const getInitialTiles = (isRandom = true): Array<TileType> => {
   const gameSet = [
