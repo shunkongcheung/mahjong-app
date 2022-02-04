b1e19fd97a32b78f68b321705158bb2c73698efd
Shun Cheung <shunkongcheung@gmail.com>
2022-02-03 17:53:23 -0800

    feat(Button): varient

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,29 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
+type Varient = "primary" | "normal";
+
 interface ButtonProps {
   children: ReactNode;
-  onClick: () => any
+  onClick: () => any;
+  varient?: Varient;
 }
 
-const Container = styled.button`
-  background: ${({ theme }) => theme.colors.primary[400]};
+const Container = styled.button<{ varient: Varient }>`
   border: 0;
+  ${({ theme, varient }) =>
+    varient === "primary"
+      ? `
   border-radius: 5px;
   color: white;
+  background: ${theme.colors.primary[400]};
+  `
+      : `
+  outline: 1px solid ${theme.colors.primary[400]};
+  background: white;
+  color: ${theme.colors.primary[400]};
+  `}
+
   padding: 5px 10px;
   margin-right: 10px;
 
@@ -18,13 +31,22 @@ const Container = styled.button`
 
   &:hover {
     cursor: pointer;
-    background: ${({ theme }) => theme.colors.primary[500]};
+    background: ${({ theme, varient }) =>
+      varient === "primary" && theme.colors.primary[500]};
     outline: 2px solid ${({ theme }) => theme.colors.primary[200]};
   }
 `;
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
-  return <Container onClick={onClick}>{children}</Container>;
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  varient = "primary",
+}) => {
+  return (
+    <Container onClick={onClick} varient={varient}>
+      {children}
+    </Container>
+  );
 };
 
 export default Button;
